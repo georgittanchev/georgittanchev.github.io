60abf75a28688b5c4e71468d6b5dc25cb2bac6d9
Georgi Tanchev <georgittanchev@gmail.com>
2020-09-15 19:35:28 +0300

    changes

--- mail.php
+++ mail.php
@@ -1,13 +1,18 @@
 <?php
-      if ($_POST['message']) {
-        $message = $_POST['message'];
-        $email = $_POST['email'];
-        $name = $_POST['name'];
-        $headers = "From: ".$name."<".$email.">";
+if(isset( $_POST['name']))
+$name = $_POST['name'];
+if(isset( $_POST['_replyto']))
+$email = $_POST['_replyto'];
+if(isset( $_POST['theMessage']))
+$message = $_POST['theMessage'];
 
-        mail('georgittanchev@gmail.com', "Contact from RRR", $message, $headers);
-      }
-    ?>
+$content="From: $name \n Email: $email \n Message: $message";
+$recipient = "georgittanchev@gmail.com";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $content, $mailheader) or die("Error!");
+
+
+?>
 
 <script>
 window.location.replace("http://georgittanchev.github.io/thanks.html");</script>
